33ed831805bf3d583857455236be966c7a906f71
Arnav Mishra <arnmishra@gmail.com>
2017-11-22 14:09:11 -0800

    asynchronous date increment

--- project/views.py
+++ project/views.py
@@ -1,11 +1,11 @@
-import datetime
 from project import app, db
 from models import Employee, Customer, Vendor, PayrollEvents, Parts, InvoiceHistory, POHistory
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, jsonify
 from project.scripts.taxes import calculate_social_security_tax, calculate_medicare_tax, calculate_federal_tax, calculate_state_tax
 
-initialized = False
-cost_of_units = 2.5
+DATE = 0
+INITIALIZED = False
+COST_OF_UNITS = 2.5
 income_statement = {"sales": 0, "cogs": 0, "payroll": 0, "payroll_withholding": 0, "bills": 0, "annual_expenses": 0,
                     "other_income": 0}
 balance_sheet = {"cash": 0, "accounts_receivable": 0, "inventory": 0, "land": 0, "equipment": 0, "furniture": 0, 
@@ -17,9 +17,9 @@ def index():
 
     :return: initialize business or home page
     """
-    global initialized
-    if not initialized:
-        initialized = True
+    global INITIALIZED
+    if not INITIALIZED:
+        INITIALIZED = True
         return redirect("/initialize_business")
     return redirect("/home")
 
@@ -58,7 +58,11 @@ def home():
 
     :return: home.html
     """
-    return render_template("home.html")
+    global DATE, INITIALIZED
+    if not INITIALIZED:
+        INITIALIZED = True
+        return redirect("/initialize_business")
+    return render_template("home.html", date=DATE)
 
 @app.route("/view_employees", methods=['GET'])
 def view_employees():
@@ -254,6 +258,7 @@ def create_po():
 
     :return: create_po.html
     """
+    global DATE
     if request.method == "GET":
         vendors = Vendor.query.all()
         return render_template("create_po.html", vendors=vendors)
@@ -270,7 +275,7 @@ def create_po():
         value = quantity * price_per_unit
         new_parts = Parts(part, vendor.price, quantity, value)
         db.session.add(new_parts)
-    date = str(datetime.date.today())
+    date = DATE
     total = quantity * price_per_unit
     new_po = POHistory(date, vendor.company, part, quantity, price_per_unit, total)
     db.session.add(new_po)
@@ -283,16 +288,23 @@ def create_invoice():
 
     :return: create_invoice.html
     """
-    global cost_of_units
+    global COST_OF_UNITS, DATE
     if request.method == "GET":
         customers = Customer.query.all()
         num_available = get_num_available()
         return render_template("create_invoice.html", customers=customers, num_available=num_available)
     customer = request.form["customer"]
     quantity = request.form["quantity"]
-    date = str(datetime.date.today())
-    total = cost_of_units * quantity
-    InvoiceHistory(date, customer, quantity, cost_of_units, total)
+    date = DATE
+    total = COST_OF_UNITS * quantity
+    InvoiceHistory(date, customer, quantity, COST_OF_UNITS, total)
+
+@app.route("/increment_date", methods=['POST'])
+def increment_date():
+    """ Asynchronously Increments the Date """
+    global DATE
+    DATE += 1
+    return jsonify({"date": DATE})
 
 def get_num_available():
     """ Gets the number of complete units available
